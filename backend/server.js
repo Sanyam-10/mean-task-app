083abad550b7c8fa87000559e28754b6801f024e
Sanyam Singh <singhsanyam420@gmail.com>
2026-02-26 00:11:23 +0530

    fix: update nginx config, frontend API URL and backend CORS

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 const express = require("express");
-//const cors = require("cors");
-
+const cors = require("cors");
 const app = express();
-
-// parse requests of content-type - application/json
+app.use(cors());
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
 const db = require("./app/models");
+const dbUrl = process.env.MONGO_URI || "mongodb://mongodb:27017/cruddb";
+
 db.mongoose
-  .connect(db.url, {
+  .connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -23,15 +20,15 @@ db.mongoose
     process.exit();
   });
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
